ef883d0179eda441d9cb2c597368c594d8d697f2
Kyle Daily <kdaily@gmail.com>
2016-01-06 14:22:11 +0000

    Added mailer and view

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -1,10 +1,12 @@
 class ContactMailer < ActionMailer::Base
+    default to: 'kdaily@gmail.com'
+
    def contact_email(name, email, body)
        @name = name
        @email = email
        @body = body
       
-       mail(from: email, subject: "Contact Form Message 31")
+       mail(from: email, subject: 'Contact Form Message 31')
    end
    
    
